35b83e457d393e3c3ef80755a318f7eaea0f39fd
nvshah286 <nvshah286@gmail.com>
2019-04-16 20:50:24 -0500

    updating the backtest class to make some methods private

--- Backtest/runBacktest.py
+++ Backtest/runBacktest.py
@@ -10,7 +10,7 @@ tickers = stockData.getTickers('SP500')
 
 # get data for those tickers
 btData = stockData.getIndexData(symbols=tickers,
-                                fileName='SP500Data', update=True)
+                                fileName='SP500Data', update=False)
 
 summary = pd.DataFrame()
 # generating the backtest object
@@ -21,7 +21,7 @@ for h in hp:
     bk1 = Backtest.BackTest(func=Volatility.bollingerlow,
                             stockData=btData,
                             arguments=None,
-                            holdingPeriod=h)
+                            holdingPeriod=5)
     s1 = bk1.summary()
     summary = summary.append(s1, ignore_index=True)
 
@@ -34,15 +34,22 @@ plt.title('Low Bollinger backtesting against SP500')
 ## Attempt to convert series into data frame
 
 retDF = pd.DataFrame()
-ser = bk1.retSeries()
+ser = bk1.btReturns
 
 for s in ser.keys():
-    retDF = retDF.append({'Symbol': s, 'ReturnSeries': ser[s], 'TradeCounts': len(ser[s])}, ignore_index=True)
+    temp = pd.DataFrame({'Symbol': s, 'ReturnSeries': ser[s]})
+    retDF = retDF.append(temp, ignore_index=True)
+
+
+## dropping Nan Values
+retDF.dropna(inplace=True)
 
 # attach the return series with the metadata csv file.
 # (this means backtest needs to know which index we are testing against)
 
-sp500metadata = pd.read_csv('DataFiles/SP500Companies.csv')[['Symbol', 'GICS Sector', ' GICS Sub Industry']]
+sp500metadata = pd.read_csv('DataFiles/SP500Companies.csv')[['Symbol', 'GICS Sector', 'GICS Sub Industry']]
 
 allData = pd.merge(retDF, sp500metadata, on='Symbol')
-allData.columns = ['RetSeries', 'Symbol', 'TradeCounts', 'GICS_Sector', 'GICS_SubIndustry']
+allData.columns = ['RetSeries', 'Symbol', 'GICS_Sector', 'GICS_SubIndustry']
+
+allData.to_csv('LowBollingerSP500Results.csv')
